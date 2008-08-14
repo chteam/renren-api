d81d7f128cc440faa3b3b0f1b09310b84abe4623
chsword_cp <SND\chsword_cp>
2008-08-14 17:22:18 +0000

    Profile的XML实体

---
+++ XiaoNei/XiaoNei/ApiContainer/Profile/GetXNML.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace XiaoNei.ApiContainer.Profile {
+
+	[XmlRoot("profile_getXNML_response", Namespace = "http://api.xiaonei.com/1.0/")]
+	public class GetXNML {
+		[XmlText]
+		public String Val { get; set; }
+	}
+}
