73decf2843445d7963a976498578c1ab5d520b49
chsword_cp <SND\chsword_cp>
2008-08-14 17:22:03 +0000

    User的XML实体

---
+++ XiaoNei/XiaoNei/ApiContainer/Users/IsAppAdded.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace XiaoNei  {
+	[XmlRoot("users_isAppAdded_response", Namespace = "http://api.xiaonei.com/1.0/")]
+	public class IsAppAdded {
+		[XmlText]
+		public int Val { get; set; }
+	}
+}
---
+++ XiaoNei/XiaoNei/ApiContainer/Users/LoggedInUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.Collections;
+
+namespace XiaoNei {
+	[XmlRoot("users_getLoggedInUser_response", Namespace = "http://api.xiaonei.com/1.0/")]
+	public class LoggedInUser {
+		[XmlText]
+		public String UID { get; set; }
+	}
+}
